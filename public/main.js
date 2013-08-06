194e6d11cda5dcd3af90ee13229069b5c6d48f20
Michel Jansen <mail@micheljansen.org>
2013-08-06 18:38:34 +0100

    first stab at adding a UI to the logger

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 function log(text) {
+  /*
   var el = document.getElementById("log");
 
   if("join" in text) {
@@ -7,17 +8,32 @@ function log(text) {
   else {
     el.innerHTML = text+"\n" + el.innerHTML;
   }
+ */
+  if(console && console.log) { console.log(text); }
 }
 
-var geo_options = {
-  enableHighAccuracy: true,
-  maximumAge        : 0,
-  timeout           : Infinity
-};
+function startLogging() {
+  if(window["humanTideLoggingEnabled"]) {
+    return;
+  }
 
-var watchID = navigator.geolocation.watchPosition(handleSuccess, handleError, geo_options);
+  window["humanTideLoggingEnabled"] = true;
+
+  var geo_options = {
+    enableHighAccuracy: true,
+    maximumAge        : 0,
+    timeout           : Infinity
+  };
+
+  var watchID = navigator.geolocation.watchPosition(handleSuccess, handleError, geo_options);
+
+  return watchID;
+}
 
 function handleSuccess(position) {
+  if(window.location.hash == "#step4") {
+    window.location.hash = "#step5";
+  }
   log([position.timestamp, position.coords.latitude, position.coords.longitude, position.coords.accuracy]);
   var xmlhttp = new XMLHttpRequest();
   var measured_time = position.timestamp > 1000000000 ? position.timestamp / 1000 : position.timestamp;
@@ -34,7 +50,56 @@ function handleSuccess(position) {
   xmlhttp.send();
 }
 
-function handleError() {
+function handleError(err) {
   //alert("error");
   log("gps error");
+  if(err.code == 1) {
+    alert("Permission denied");
+  }
 }
+
+function showPage() {
+  var hash = window.location.hash;
+  switch(hash) {
+    case "#step2":
+      $('#step1, #step3, #step4, #step5, #done').fadeOut('fast');
+      $('#step2').fadeIn('fast'); 
+      break;
+    case "#step3":
+      $('#step1, #step2, #step4, #step5, #done').fadeOut('fast');
+      $('#step3').fadeIn('fast'); 
+      break;
+    case "#step4":
+      $('#step1, #step2, #step3, #step5, #done').fadeOut('fast');
+      $('#step4').fadeIn('fast');
+      startLogging();
+      break;
+    case "#step5":
+      $('#step1, #step2, #step3, #step4, #done').fadeOut('fast');
+      $('#step5').fadeIn('fast');
+      break;
+    case "#done":
+      $('#step1, #step2, #step3, #step4, #step5').fadeOut('fast');
+      $('#done').fadeIn('fast'); 
+      startLogging();
+      break;
+    default:
+      $('#step2, #step3, #step4, #step5, #done').fadeOut('fast');
+      $('#step1').fadeIn('fast'); 
+  }
+  $('html, body').animate({ scrollTop: 0 }, 10);
+}
+
+window.addEventListener("hashchange", showPage, false);
+
+// if someone refreshes past step 4, bump them back to the "waiting" dialog.
+if(window.location.hash == "#step5" || window.location.hash == "#done" ) {
+  window.location.hash = "#step4";
+}
+// run showPage on page load
+$(showPage);
+
+$('a').css('cursor', 'pointer');
+
+
+
