d60f6efb1b246865fce42031b357b9904ac82e6c
Michel Jansen <mail@micheljansen.org>
2013-08-09 11:57:56 +0100

    sanitise input

--- web.js
+++ web.js
@@ -97,10 +97,19 @@ else {
     with_client_do(request, response, function(client) {
       var clientid = client.id;
       var q = request.body;
+
+      var fix = function(x) {
+        var ret = parseFloat(x);
+        return isNaN(ret) ? null : ret;
+      }
+
+      var data = [clientid,
+        fix(q["t"]) , fix(q["t"]), fix(q["lat"]), fix(q["long"]), fix(q["acc"]), fix(q["alt"]), fix(q["alt_acc"]), null];
+      console.log(data);
       pgc.query("INSERT INTO datapoints(\
                 client_id, created_at, sent_at, generated_at, latitude, longitude, accuracy, altitude, altitude_accuracy, debug)\
                 VALUES($1, now(), to_timestamp($2), to_timestamp($3), $4, $5, $6, $7, $8, $9) RETURNING *",
-                [clientid, q["t"], q["t"], q["lat"], q["long"], q["acc"], q["alt"], q["alt_acc"], null],
+                data,
                 function(err, row) {
                   response.write(JSON.stringify([err,row], null, 2));
                   response.end();
