c0f80738432ab06e9c49740566d1081342866925
micheljansen <mail@micheljansen.org>
2013-09-15 21:48:33 +0100

    use segmented display

--- public/show.js
+++ public/show.js
@@ -29,41 +29,37 @@ var funkycolors = ["blueviolet", "chartreuse", "darkblue", "darkmagenta", "green
 var params = getUrlVars();
 var $debug = $("#debug");
 
-$.ajax({dataType: "JSON", url: "/trails.json?"+ params })
-.done(function(result) {
+$.ajax({dataType: "JSON", url: "/segments.json?"+ params })
+.done(function(segments) {
   var bounds = new L.LatLngBounds();
 
-  var grouped = result.reduce(function(coll, e, i, array){
-    coll[e.client_id] = coll[e.client_id] ? coll[e.client_id] : [];
-    coll[e.client_id].push(e);
-    return coll;
-  }, {});
-
-  for (var key in grouped) {
-    var trail = grouped[key];
-    var latlngs = trail.map(function(e) { return new L.LatLng(+e.latitude, +e.longitude) });
-    bounds.extend(latlngs);
-    //var pl = new L.Polyline(latlngs, {color: funkycolors[key % funkycolors.length]}).addTo(map)
-    var pl = new L.Polyline(latlngs, {color: "black"}).addTo(map)
-    var dots = trail.map(function(e) {
-      var dt =  new Date(e.generated_at);
-      var minute = dt.getMinutes();
-      var t = (dt.getMinutes()*60+dt.getSeconds())/(60*60);
-      var dot = new L.Circle(new L.LatLng(+e.latitude, +e.longitude), 0.5 /*e.accuracy*/,
-      {
-        //color: funkycolors[key & funkycolors.length],
-        color: "hsl("+Math.floor(360*t)+",100%, 50%)",
-        //stroke: false,
-        weight: 2,
-        opacity: 1,
-        fillOpacity: 1,
-        title: "test"
+   _(segments).each(function(segment) {
+    for (var cid in segment) {
+      var trail = segment[cid];
+      var latlngs = trail.map(function(e) { return new L.LatLng(+e.latitude, +e.longitude) });
+      bounds.extend(latlngs);
+      //var pl = new L.Polyline(latlngs, {color: funkycolors[cid % funkycolors.length]}).addTo(map)
+      var pl = new L.Polyline(latlngs, {color: "black"}).addTo(map)
+      var dots = trail.map(function(e) {
+        var dt =  new Date(e.generated_at);
+        var minute = dt.getMinutes();
+        var t = (dt.getMinutes()*60+dt.getSeconds())/(60*60);
+        var dot = new L.Circle(new L.LatLng(+e.latitude, +e.longitude), 0.5 /*e.accuracy*/,
+        {
+          //color: funkycolors[cid & funkycolors.length],
+          color: "hsl("+Math.floor(360*t)+",100%, 50%)",
+          //stroke: false,
+          weight: 2,
+          opacity: 1,
+          fillOpacity: 1,
+          title: "test"
+        });
+        // dot.addTo(map);
+        dot.on('mouseover', function() { $debug.html(JSON.stringify(e, null, "  ")); console.log(e) });
+        return dot;
       });
-      dot.addTo(map);
-      dot.on('mouseover', function() { $debug.html(JSON.stringify(e, null, "  ")); console.log(e) });
-      return dot;
-    });
-  }
+    }
+  });
 
   map.fitBounds(bounds);
 
